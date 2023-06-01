c03d00513fe5b8091357e86e5066b0fa64e5b886
Sudip Ghimire <sudipghimire533@gmail.com>
2023-06-01 14:47:44 +0200

    Add feature to use live or local metadata

--- src/chain/metadata.rs
+++ src/chain/metadata.rs
@@ -1,2 +1,13 @@
+#[cfg_attr(
+    not(all(feature = "saved-metadata", not(feature = "live-metadata"))),
+    cfg(target_family = "never")
+)]
 #[::subxt::subxt(runtime_metadata_path = "res/edu_chain_metadata.scale")]
+pub mod edu_chain {}
+
+#[cfg_attr(
+    not(all(feature = "live-metadata", not(feature = "saved-metadata"))),
+    cfg(target_family = "never")
+)]
+#[::subxt::subxt(runtime_metadata_url = "ws://127.0.0.1:9944")]
 pub mod edu_chain {}
